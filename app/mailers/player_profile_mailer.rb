eac263b52e26d6198f18549b356c877d7f52e9a6
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-31 00:52:32 -0500

    Added player profile mailer

---
+++ app/mailers/player_profile_mailer.rb
@@ -0,0 +1,2 @@
+class PlayerProfileMailer < ApplicationMailer
+end
---
+++ test/mailers/previews/player_profile_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/player_profile_mailer
+class PlayerProfileMailerPreview < ActionMailer::Preview
+
+end
