0e4332ff0b155771a2102ce02d1a4e8131fe002e
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-03-31 00:58:04 -0500

    added welcome email

--- app/controllers/player_profiles_controller.rb
+++ app/controllers/player_profiles_controller.rb
@@ -18,6 +18,8 @@ class PlayerProfilesController < ApplicationController
     @player_profile = PlayerProfile.new(player_profile_params)
 
     if @player_profile.save
+      # Call the UserMailer to send a welcome email after save
+      PlayerProfileMailer.welcome_email(@player_profile).deliver_later
       render json: @player_profile, status: :created, location: @player_profile
     else
       render json: @player_profile.errors, status: :unprocessable_entity
@@ -46,6 +48,6 @@ class PlayerProfilesController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def player_profile_params
-      params.fetch(:player_profile, {})
+        params.require(:player_profile).permit(:pp_username, :email, :password, :location_id)
     end
 end
--- app/mailers/player_profile_mailer.rb
+++ app/mailers/player_profile_mailer.rb
@@ -1,2 +1,10 @@
 class PlayerProfileMailer < ApplicationMailer
+  default from: "'SPairing' <spairing.un@gmail.com>"
+
+  def welcome_email(player_profile)
+    @url  = "http://spairing.herokuapp.com/"
+    @player_profile = player_profile
+    mail  to: @player_profile.email,
+          subject: "Welcome to SPairing"
+  end
 end
