d06a278f75b3d76d93ea1b9435dd846739234e34
Andres Gonzalez <adgonzalezo@unal.edu.co>
2018-04-17 01:03:45 -0500

    new google authenticate route

--- app/controllers/player_profile_token_controller.rb
+++ app/controllers/player_profile_token_controller.rb
@@ -1,23 +1,3 @@
-require httparty
-
 class PlayerProfileTokenController < Knock::AuthTokenController
-  include HTTParty
-  
-  def google_authentication
-    token = params["id_token"]
-    response = HTTParty.get('https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=#{token}')
-    
-    puts response
-    puts response.parsed_response
-    
-    comparo 2 cadenas
-      devulvo el token
-      self.create
-    si no
-      no devuelvo nada
-    
-  end
-  
-  
-  
+
 end
---
+++ test/controllers/social_auth_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SocialAuthControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
